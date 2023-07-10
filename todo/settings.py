57aa41e6cd2b04c14f3845b0c0f5bfef6460870a
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2023-07-11 02:16:39 +0330

    Fix "," in install_apps

--- todo/settings.py
+++ todo/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party
-    'rest_framework'
+    'rest_framework',
     # local
     'todos.apps.TodosConfig',
 
