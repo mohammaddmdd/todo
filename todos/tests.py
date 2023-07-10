4d9a49ae9aaf28924df94c5b715ba61a3e7ab1c5
mohammad.darvishi <mohammaddarvishi5@gmail.com>
2023-07-11 02:18:32 +0330

    Test DetailTodo,Todolist views

--- todos/tests.py
+++ todos/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
 from todos.models import Todo
 
@@ -14,3 +16,15 @@ class TodoTest(TestCase):
         self.assertEqual(self.todo_test.title, 'Test title')
         self.assertEqual(self.todo_test.body, 'Test body')
         self.assertEqual(str(self.todo_test), 'Test title')
+
+    def test_todo_list(self):
+        response = self.client.get(reverse('todo_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, 'Test title')
+        self.assertEqual(Todo.objects.count(), 1)
+
+    def test_todo_detail(self):
+        response = self.client.get(reverse('todo_detail', kwargs={'pk': self.todo_test.id}), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, 'Test title')
